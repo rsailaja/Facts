ff577a430cc90adc2edfccf713a5904f7cc00ec6
rsailaja <rsailaja@hotmail.com>
2018-02-28 14:00:26 +1100

    Added Models

--- Facts.swift
+++ Facts.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+struct Facts: Codable {
+    let title: String
+    let rows: [Row]
+}
+
+struct Row: Codable {
+    let title: String
+    let description: String
+    let imageHref: String
+}
